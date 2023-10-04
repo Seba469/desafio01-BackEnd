6088cb88dabda4dac53a08275e2e57970ce73a7f
Seba Viberti <sebaviberti2002@gmail.com>
2023-10-04 20:22:29 -0300

    code agregado

--- productManager.js
+++ productManager.js
@@ -7,35 +7,43 @@ class ProductManager{
         return this.products
     }
 
-    addProducts(title, description, price, thumbnail, stock){
-        if(!title, !description, !price, !thumbnail, !stock){
+    addProducts(title, description, price, thumbnail, code, stock){
+        if(!title, !description, !price, !thumbnail, !code, !stock){
             console.log("Debe completar todos los campos")
             return;
         }
 
         const newProduct = {
-            code: this.products.length + 1,
+            id: this.products.length + 1,
             title,
             description,
             price,
             thumbnail,
+            code,
             stock,
         };
 
+        const index = this.products.findIndex(p => p.code === code )
+
+        if (index != -1){
+            console.log("Codigo repetido")
+            return;
+        }
+        
         this.products.push(newProduct)
         console.log("Producto agregado")
         return;
+        
     }
 
-    getProductsById(code){
-        const productoFiltrado = this.products.filter(p => p.code === code)
+    getProductsById(id){
+        const productoFiltrado = this.products.filter(p => p.id === id)
 
         if (productoFiltrado.length === 0){
             console.log("Not found")
             return
         }else{
-            console.log(productoFiltrado)
-            return 
+            return productoFiltrado
         }  
     }
 }
@@ -44,9 +52,9 @@ const productManager = new ProductManager();
 
 console.log("getProducts", productManager.getProducts())
 console.log("==================================================================")
-productManager.addProducts("pantalon", "pantalon negro", 120, "pantalonNegro.jpg", 30)
-productManager.addProducts("remera", "remera roja", 120, "remeraRoja.jpg", 30)
+productManager.addProducts("pantalon", "pantalon negro", 120, "pantalonNegro.jpg", "cod1", 30)
+productManager.addProducts("remera", "remera roja", 120, "remeraRoja.jpg", "cod2", 30)
 console.log("==================================================================")
 console.log("getProducts", productManager.getProducts())
 console.log("==================================================================")
-console.log("getProductsById", productManager.getProductsById(5))
+console.log("getProductsById", productManager.getProductsById(1))
